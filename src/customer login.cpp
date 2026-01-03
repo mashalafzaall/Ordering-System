e02d6479feee2989f9ce49760bcaec5e000eced3
Zainab-Naeem <9577366@gmail.com>
2026-01-03 18:58:26 +0500

    Create customer login.cpp

---
+++ src/customer login.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+     ifstream in("orders.txt");   // Open the file orders.txt for reading
+
+    int customerpass,itemnum,quantity,bill;
+    string cutomeruser;
+    while(in>>cutomeruser>>customerpass>>itemnum>>quantity>>bill){
+    cout<<" "<<cutomeruser<<" "<<customerpass<<" "<<itemnum<<" "<<quantity<<" "<<bill<<endl;
+    }
+   in.close();
+    return 0;
+}
