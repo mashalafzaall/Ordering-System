a7cbf084f8dc13cd0e33c8cd57b225692f5fe1e9
humna56 <humnahx9@gmail.com>
2026-01-03 17:57:25 +0500

    Refactor file stream initialization for clarity

---
+++ src/Customer registration.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main() {
+    int customerpass;
+    string customeruser;
+  //Taking username from user
+    cout<<"Enter your username:";
+    cin>>customeruser;
+    cout<<"Enter your password:";
+    cin>>customerpass;
+    ofstream file("customer.txt",ios::app);
+    file<<customeruser<<""<<customerpass<<""<<endl;
+    file.close();
+    cout<<"Registration successful"<<endl;
+    return 0;
+}
